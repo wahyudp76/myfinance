534857dafa55dc749de5b793ebb8e19d27986ba2
wahyudp76 <145712332+wahyudp76@users.noreply.github.com>
2026-08-21 06:13:48 +0700

    refactor: expose auth lifecycle controller

---
+++ src/auth/index.js
@@ -0,0 +1,4 @@
+export { initAuthClient, getAuthClient } from "./client.js";
+export { getSession, getCurrentUser, signIn, signUp, signOut } from "./session.js";
+export { onAuthStateChange, requireUser } from "./guards.js";
+export { createAuthLifecycle } from "./lifecycle.js";
