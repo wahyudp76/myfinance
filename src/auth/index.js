71e1a2014fccf161de762e573a6a2f3d65c39b4e
wahyudp76 <145712332+wahyudp76@users.noreply.github.com>
2026-08-21 06:06:56 +0700

    refactor: expose auth boundary

---
+++ src/auth/index.js
@@ -0,0 +1,3 @@
+export { initAuthClient, getAuthClient } from "./client.js";
+export { getSession, getCurrentUser, signIn, signUp, signOut } from "./session.js";
+export { onAuthStateChange, requireUser } from "./guards.js";
