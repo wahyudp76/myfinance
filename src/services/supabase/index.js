87aff0f29d54f591ab558180a192501a24165018
wahyudp76 <145712332+wahyudp76@users.noreply.github.com>
2026-08-21 05:35:08 +0700

    refactor: expose complete supabase service layer

--- src/services/supabase/index.js
+++ src/services/supabase/index.js
@@ -1,1 +1,5 @@
+export { createSupabaseBrowserClient } from "./client.js";
 export * from "./transactions.js";
+export * from "./transfers.js";
+export * from "./recurring.js";
+export * from "./budgets.js";
